e6b822b6dfd8570069e79df24656f169517e0e41
Rhishi kesh <reshikash300@gmail.com>
2024-07-11 22:57:09 +0600

    Text Reveal Animation & Project Complete

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ navTrigerBtn.onclick = () => {
     nav_menu.classList.toggle('active-nav');
 }
 
-//Testimonail
+//Testimonail Slider
 const swiper = new Swiper('.swiper', {
     loop: true,
     pagination: {
@@ -27,3 +27,43 @@ const swiper = new Swiper('.swiper', {
         }
     }
 })
+
+//Scroll Reveal Animation
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 3000,
+    delay: 600,
+    reset: true,
+})
+
+//Hero
+sr.reveal('.hero__text', {origin: 'top'});
+//Steps
+sr.reveal('.steps__step', {distance: '100px', interval: 100});
+//About
+sr.reveal('.about__text', {origin: 'left'});
+sr.reveal('.about__img', {origin: 'right', delay: 800,});
+//Testimonial
+sr.reveal('.testimonial__bg', {delay: 800,});
+sr.reveal('.testimonial__title');
+sr.reveal('.testimonial__slider', {delay: 1000});
+//brands
+sr.reveal('.brand__img', {distance: '100px', interval: 100});
+// work
+sr.reveal('.work__title');
+sr.reveal('.work__subtitle', {delay: 800,});
+sr.reveal('.work__grid', {distance: '100px', interval: 100});
+//stars
+sr.reveal('.stats');
+sr.reveal('.stats__item', {distance: '100px', interval: 100});
+//news
+sr.reveal('.news__title');
+sr.reveal('.news__subtitle', {delay: 800,});
+sr.reveal('.news__grid', {distance: '100px', interval: 100, delay: 1000});
+//contact
+sr.reveal('.contact__container');
+sr.reveal('.contact__text', {delay: 800,});
+//footer
+sr.reveal('.footer__item', {distance: '100px', interval: 100, delay: 1000});
+sr.reveal('.footer__copyright');
